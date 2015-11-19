6e56b0ce66a8a7657c18f43e6a628ae467e82458
AircityScript <842736610@qq.com>
2015-11-19 15:35:32 +0800

    commit

--- client/views/companyInfo/main_form.js
+++ client/views/companyInfo/main_form.js
@@ -11,8 +11,46 @@ Template.scope_segement.events({
   }
 });
 
-Template.scope_segement.helpers({
-  '_dynamic':function(){
+Template.form.helpers({
+  _dynamic:function(){
         return 'scope_segement';  
   }
 });
+
+Template.scope_segement.helpers({
+  pageNames:function(){
+    var pageNames = [{
+      name: 'scope_segement_board'
+    }, {
+      name: 'scope_segement_widget1'
+    }, {
+      name: 'scope_segement_widget2'
+    }, {
+      name: 'scope_segement_widget3'
+    }];
+    return pageNames;
+  }
+});
+
+Template.scope_segement.onRendered(function(){
+    var atuoSwiper = new Swiper ('.swiper-container', {
+//      direction: 'vertical',
+      loop: false
+    });
+    $("#step").click(function(){
+       atuoSwiper.slideNext();
+       return false;
+    });
+    
+    $(".scope_segement_widget1 .module").click(function(){
+       atuoSwiper.slideNext();
+       return false;
+    });
+  
+    $(".scope_segement_widget2 .module").click(function(){
+       atuoSwiper.slideNext();
+       return false;
+    });  
+  
+})
+
