89a6497e25c7f27e213f1044c7499ae432e25122
air-cc <c989692c@gmail.com>
2016-04-29 18:18:41 +0800

    sms log

--- server/sms/smsSend.js
+++ server/sms/smsSend.js
@@ -40,6 +40,8 @@ SMSSend.send = function (phone, message, tmplStr, callback) {
 
   var params = SMSSend.getParams();
 
+  log("send sms to " + phone + " @" + phone);
+
   HTTP.call("POST", params.url,
     {
       "data":{
